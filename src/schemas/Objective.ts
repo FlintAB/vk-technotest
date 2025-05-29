3269a6387db30958fb5aa3aef5807334c038d145
Aleksey Baranovskiy <lari.bro@yandex.ru>
2025-05-29 19:05:27 +0300

    fix/Form schema

--- src/schemas/Objective.ts
+++ src/schemas/Objective.ts
@@ -3,21 +3,25 @@ import { validateDates } from "../utils/validateDates";
 
 export const ObjectiveSchema = z.object({
    title: z.string()
+      .nonempty('Поле title не может быть пустым')
       .min(3, "Минимальная длина для title 3")
       .max(50, "Максимальная длина для title 50"),
 
    description: z.string()
+      .nonempty('Поле description не может быть пустым')
       .min(10, "Минимальная длина для description 10")
       .max(500, "Максимальная длина для description 500"),
 
    dateStart: z.union([
       z.date(),
       z.string()
+      .nonempty('Стартовая дата обязательна')
    ]),
 
    dateEnd: z.union([
       z.date(),
       z.string()
+      .nonempty('Конечная дата обязательна')
    ]),
 
    priority: z.enum(["low", "medium", "high"]),
@@ -34,12 +38,15 @@ export const ObjectiveSchema = z.object({
       ])
    ).optional()
 
-}).refine(data => {
+}).superRefine((data, ctx) => {
    const error = validateDates(data.dateStart, data.dateEnd);
-   return error === null;
-}, {
-   message: "Ошибка при инициализации дат, проверьте стартовую и конечную дату",
-   path: ["dateEnd"]
-});
+   if (error !== null){
+      ctx.addIssue({
+         code: z.ZodIssueCode.custom,
+         message: error,
+         path: ['dateEnd'],
+      })
+   }
+})
 
 export type TObjectiveForm = z.infer<typeof ObjectiveSchema>;
