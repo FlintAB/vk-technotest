0efa209f7d20f48dde21880f730b7c8b03e3f795
Aleksey Baranovskiy <lari.bro@yandex.ru>
2025-05-31 21:33:52 +0300

    add/Module Styles for AddFieldform.tsx

--- src/components/field/AddFieldForm.tsx
+++ src/components/field/AddFieldForm.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import type { AddFieldFormProps } from "../../types/Props";
-
+import styles from './AddFieldForm.module.css';
 
 export const AddFieldForm: React.FC<AddFieldFormProps> = ({
    newFieldKey,
@@ -15,14 +15,14 @@ export const AddFieldForm: React.FC<AddFieldFormProps> = ({
    };
 
    return (
-      <div>
-         <h3>Добавить новое поле</h3>
-         <input type="text" value={newFieldKey} placeholder="Введите наименование поля" onChange={handleFieldFormChange} />
-         <div>
-            <button onClick={onSubmit} disabled={isDisabled}>
+      <div className={styles.fieldForm}>
+         <h3 className={styles.title}>Добавить новое поле</h3>
+         <input type="text" value={newFieldKey} placeholder="Введите наименование поля" onChange={handleFieldFormChange}  className={styles.fieldInput}/>
+         <div className={styles.fieldActions}>
+            <button onClick={onSubmit} disabled={isDisabled} className={styles.addButton}>
                Создать поле
             </button>
-            <button onClick={onCancel}>
+            <button onClick={onCancel} className={styles.cancelButton}>
                Свернуть форму
             </button>
          </div>
